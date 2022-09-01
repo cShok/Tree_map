54d5913d6188f6ce9f677a69e1162dfe9d4f8e0f
chanay <chananyshok@gmail.com>
2022-09-01 16:57:02 +0300

    add marker to db

--- app/src/main/java/com/example/maptur/MapsActivity.java
+++ app/src/main/java/com/example/maptur/MapsActivity.java
@@ -4,9 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,6 +29,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private ActivityMapsBinding binding;
     private FirebaseAuth mAuth;
     private FirebaseFirestore db; // private??
+    private boolean signedIn = true;
+
+    private Button notSignedButton;
+    private TextView notSignedText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,8 +70,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//      mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//      mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.setOnMarkerClickListener(this::onMarkerClick);
         mMap.setOnMapLongClickListener(this::onMapLongClick);
 
@@ -78,9 +84,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // check if in a public space
         // pop the quiz qustion, first, do you wanna add tree...
         // add a listner or once retrive the data
+
+        if (!signedIn) { //pop-up/ sign in please?
+            notSignedButton = (Button) findViewById(R.id.NotSigned);
+            notSignedText = (TextView) findViewById(R.id.NotSignedText);
+
+            notSignedText.setVisibility(View.VISIBLE);
+            new Handler().postDelayed(() ->  notSignedText.setVisibility(View.INVISIBLE), 3000);
+            return;
+        }
+        MarkerOptions newMarker = new MarkerOptions();
+        // Setting the position for the marker
+        newMarker.position(latLng);
+        // Setting the title for the marker.
+        // This will be displayed on taping the marker
+        newMarker.title(latLng.latitude + " : " + latLng.longitude);
+
+        // Placing a marker on the touched position
+
+
         mMap.addMarker(new MarkerOptions().position(latLng));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        db.collection("markers").document("first");
+        // after validation, add marker to the db
+        db.collection("markers").add(newMarker);
 
     }
     public boolean onMarkerClick(final Marker marker) {
@@ -97,7 +123,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             " has been clicked " + clickCount + " times.",
                     Toast.LENGTH_SHORT).show();
         }
-        Log.i("makrer count" , "" + clickCount);
+        Log.i("makrer count **" , "**********" + clickCount);
         // Return false to indicate that we have not consumed the event and that we wish
         // for the default behavior to occur (which is for the camera to move such that the
         // marker is centered and for the marker's info window to open, if it has one).
